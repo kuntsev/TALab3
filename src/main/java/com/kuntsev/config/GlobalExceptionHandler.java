1bc003474f86742948ad305bb7a5580fa6688fa7
kuntsev <kuntsev@ukr.net>
2023-03-16 22:06:50 +0200

    1

--- src/main/java/com/kuntsev/config/GlobalExceptionHandler.java
+++ src/main/java/com/kuntsev/config/GlobalExceptionHandler.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
- * Global exception handler
+ * Global exception handler 1
  */
 @ControllerAdvice
 public class GlobalExceptionHandler {
