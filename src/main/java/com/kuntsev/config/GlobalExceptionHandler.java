7c7a2afd3e90e4e0137338588ef612abf50664ac
kuntsev <kuntsev@ukr.net>
2023-03-16 22:37:46 +0200

    44

--- src/main/java/com/kuntsev/config/GlobalExceptionHandler.java
+++ src/main/java/com/kuntsev/config/GlobalExceptionHandler.java
@@ -25,8 +25,7 @@ public class GlobalExceptionHandler {
         ModelAndView modelAndView = new ModelAndView("/error");
         String errorMessage = (throwable != null ? throwable.toString() : "Unknown error");
         modelAndView.addObject("errorMessage", errorMessage);
-        Integer 1t = new Integer (5);
-        return modelAndView;
+
 
 
     }
